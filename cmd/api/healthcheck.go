4eef9eba4d37910e5a50bb70ff562c94b27d7440
ng1n3 <code.with.muyiwa@gmail.com>
2025-04-05 23:50:00 +0100

    chore: eveloping data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,15 +5,17 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-  data := map[string]string {
-    "status": "available",
-    "environment": app.config.env,
-    "version": version,
-  }
+	env := envelope{
+		"status": "available",
+		"system_inf": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
 
-  err := app.writeJSON(w, http.StatusOK, data, nil)
-  if err != nil {
-    app.logger.Println(err)
-    http.Error(w, "This server encountered a problem and could not process your request", http.StatusInternalServerError)
-  }
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "This server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
