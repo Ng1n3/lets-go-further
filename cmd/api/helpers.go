d8df4fe3dde4351c7e9aab0015f3111cbd7a56e0
ng1n3 <code.with.muyiwa@gmail.com>
2025-04-06 14:20:28 +0100

    feat: update app with error handlers

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -22,21 +25,77 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter , status int, data envelope, headers http.Header) error {
-  js , err := json.MarshalIndent(data, "", "\t")
-  if err != nil {
-    return err
-  }
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
 
-  js = append(js, '\n')
+	js = append(js, '\n')
 
-  for key, value := range headers {
-    w.Header()[key] = value
-  }
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(status)
-  w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
 
-  return nil
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+
+	maxByte := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxByte))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+			// If the JSON contains a field which cannot be mapped to the target destination
+			// then Decode() will now return an error message in the format "json: unknown
+			// field "<name>"".
+		case strings.HasPrefix(err.Error(), "json:unkown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkown field ")
+			return fmt.Errorf("body contains unkown keys %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must be larger than %d bytes", maxByte)
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+	return nil
+
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,10 +6,38 @@ import (
 	"time"
 
 	"github.com/Ngn1n3/lets-go-further/internal/data"
+	"github.com/Ngn1n3/lets-go-further/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+  v := validator.New()
+
+  if data.ValidateMovie(v, movie); !v.Valid() {
+    app.failedValidationResponse(w, r, v.Errors)
+    return
+  }
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,6 +59,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)                                                               
+		app.serverErrorResponse(w, r, err)
 	}
 }
