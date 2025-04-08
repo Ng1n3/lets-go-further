4ded4404bed8a045c250d1efbb96caa126e7f67b
ng1n3 <code.with.muyiwa@gmail.com>
2025-04-08 23:58:59 +0100

    feat: get a movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/Ngn1n3/lets-go-further/internal/data"
 	"github.com/Ngn1n3/lets-go-further/internal/validator"
@@ -60,14 +60,16 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casblanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
-	}
+	movie , err := app.models.Movies.Get(id)
+  if err != nil {
+    switch {
+    case errors.Is(err, data.ErrRecordNotFound):
+      app.notFoundResponse(w, r)
+    default:
+      app.serverErrorResponse(w, r, err)
+    }
+    return
+  }
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
